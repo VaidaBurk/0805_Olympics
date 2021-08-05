ef35aa838868e9e4cc95c1f8e7303601d1ed9e5d
VaidaBurk <v.burkauskaite@gmail.com>
2021-08-05 20:28:00 +0300

    Initial commit

---
+++ Olympics/Controllers/AthleteController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Olympics.Models;
+using Olympics.Services;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Olympics.Controllers
+{
+    public class AthleteController : Controller
+    {
+        private readonly SqlConnection _connection;
+        private AthleteDBService _athleteDBService;
+        public AthleteController(SqlConnection connection, AthleteDBService athleteDBService)
+        {
+            _connection = connection;
+            _athleteDBService = athleteDBService;
+        }
+
+        public IActionResult List()
+        {
+            List<AthleteModel> athletes = _athleteDBService.GetData(_connection);
+            return View(athletes);
+        }
+
+        public IActionResult DisplayCreate()
+        {
+            return View("Create");
+        }
+
+        public IActionResult Create(AthleteModel athlete)
+        {
+            _athleteDBService.SaveToDatabase(athlete, _connection);
+            return RedirectToAction("List");
+        }
+
+
+    }
+}
---
+++ Olympics/Services/AthleteDBService.cs
@@ -0,0 +1,42 @@
+﻿using Olympics.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Olympics.Services
+{
+    public class AthleteDBService
+    {
+        
+        public List<AthleteModel> GetData(SqlConnection connection)
+        {
+            List<AthleteModel> athletes = new();
+            connection.Open();
+            using var command = new SqlCommand("SELECT * FROM Athletes", connection);
+            using var reader = command.ExecuteReader();
+            while (reader.Read())
+            {
+                athletes.Add(new()
+                {
+                    Id = reader.GetInt32(0),
+                    Name = reader.GetString(1),
+                    Surname = reader.GetString(2),
+                    Country = reader.GetString(3)
+                });
+            }
+            connection.Close();
+            return (athletes);
+        }
+
+        public void SaveToDatabase(AthleteModel athlete, SqlConnection connection)
+        {
+            connection.Open();
+            SqlCommand command = new($"INSERT INTO Athletes (Name, Surname, Country) VALUES ('{athlete.Name}', '{athlete.Surname}', '{athlete.Country}');", connection);
+            command.ExecuteNonQuery();
+            connection.Close();
+        }
+
+    }
+}
